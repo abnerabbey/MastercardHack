b550b1917ffc6cc97e3562467ad5474e6acc47e1
Abner Castro <abner.abbey@hotmail.com>
2015-08-01 17:21:27 -0500

    First commit for the Hackathon mastercard

---
+++ Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  Mastercard
+//
+//  Created by Abner Castro Aguilar on 01/08/15.
+//  Copyright (c) 2015 Abner Castro Aguilar. All rights reserved.
+//
+
+#ifndef Mastercard_Bridge_h
+#define Mastercard_Bridge_h
+
+
+#endif
