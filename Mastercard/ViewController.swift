64cd85f82e04cb55542037a762c667053a49e2c3
Abner Castro <abner.abbey@hotmail.com>
2015-08-01 17:19:22 -0500

    Initial Commit

---
+++ Mastercard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mastercard
+//
+//  Created by Abner Castro Aguilar on 01/08/15.
+//  Copyright (c) 2015 Abner Castro Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
