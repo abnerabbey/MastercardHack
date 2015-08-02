423ae9cec9149c6bbca581b194a653ff60985f60
Jonathan Velazquez <JohnnySiker@customer-187-174-218-184.uninet-ide.com.mx>
2015-08-02 02:58:09 -0500

    Header con Pager 1/2

--- Mastercard/FeaturedController.swift
+++ Mastercard/FeaturedController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class FeaturedController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class FeaturedController: UIViewController,UITableViewDelegate,UITableViewDataSource,UIPageViewControllerDataSource,UIPageViewControllerDelegate {
 
     
     
     @IBOutlet weak var tb_featured: UITableView!
+    
+    
     var subtitles:[String]! = []
     var tittles:[String]! = []
     var cover:[String]! = []
+    var pageTittle:[String] = []
+    var pageViewController: UIPageViewController!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,8 +29,27 @@ class FeaturedController: UIViewController,UITableViewDelegate,UITableViewDataSo
         
         tittles = ["Top 10 Cumbia Latina", "Mariachi desde Garibaldi","Indie Rock la nueva era!","Ritmo popero","Trovandooo","Rock: evolucion atraves del tiempo"]
         cover = ["mc-cover-cumbia","mc-cover-mariachi","mc-cover-indie","mc-cover-pop","mc-cover-trova","mc-cover-rock"]
+        
+        
         tb_featured.delegate = self
         tb_featured.dataSource = self
+        
+        
+        
+        pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PVC") as! UIPageViewController
+        var initViewController = self.getViewControllerByIndex(0) as! HeaderTableController
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        var vcArray = [initViewController]
+        pageViewController.setViewControllers(vcArray, direction: .Forward, animated: true, completion: nil)
+        
+        pageViewController.view.frame = CGRect(x: 0, y: 0, width: 400, height: 300)
+        self.addChildViewController(pageViewController)
+
+        
+        var v = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
+        v.backgroundColor = UIColor.redColor()
+        tb_featured.tableHeaderView = v
         // Do any additional setup after loading the view.
     }
 
@@ -56,6 +81,24 @@ class FeaturedController: UIViewController,UITableViewDelegate,UITableViewDataSo
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 80
     }
+    
+
+    
+    func getViewControllerByIndex(index:Int) -> UIViewController{
+        
+        
+        if ((self.pageTittle.count == 0) || (index >= self.pageTittle.count)) {
+            return HeaderTableController()
+        }
+        
+        var vc = self.storyboard?.instantiateViewControllerWithIdentifier("HeaderTableController") as! HeaderTableController
+        vc.index = index
+        vc.tittle =  "Titulo"
+        vc.subtitle = "Subtitulo"
+        vc.image = "mc-cover-mariachi"
+        return vc
+    }
+
     
 }
 
